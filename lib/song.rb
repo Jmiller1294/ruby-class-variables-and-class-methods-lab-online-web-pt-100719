101b0b2e9310a5cc90e216d77a794d1c4eddbbf3
Justin Miller <jayman1294@gmail.com>
2019-11-03 23:06:27 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+class Song 
+  
+  attr_accessor :name , :artist , :genre
+  
+  @@count = 0
+  @@artists = []
+   @@genres = []
+   @@genre_count = {}
+  
+def initialize(name, artist, genre) 
+  @name = name 
+  @artist = artist 
+  @genre = genre
+  @@count += 1
+  @@artists << @artist
+  @@genres << @genre
+end
+
+def self.count
+  @@count
+end
+
+
+def self.genres 
+ return @@genres.uniq
+end
+
+def self.artists
+  return @@artists.uniq
+end
+
+def self.genre_count 
+
+  
+  @@genres.each do |genre, songs|
+    songs = 0
+   
+   if @@genre_count.include?(genre)
+     songs += 1
+    else
+      @@genre_count[genre] = songs
+   end   
+ @@genre_count
+end
+end
+
+
+end
